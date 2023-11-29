660d2e594a54362e68b12a722a05d6e06e5aeb12
Alisson Alves <alissonsilabas@gmail.com>
2023-11-29 13:34:20 -0300

    added header, forms and respective css's.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Form from './components/Form';
+import Banner from './components/Banner';
+
+function App() {
+	return (
+		<>
+			<Banner />
+			<Form />
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,11 @@
+import './Banner.css';
+
+function Banner() {
+	return (
+		<header className={'banner'}>
+			<img src={'public/images/banner.webp'} alt={'Banner Principal da página'} />
+		</header>
+	);
+}
+
+export default Banner;
